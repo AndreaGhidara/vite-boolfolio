5ab44ced00694f248240fe259ebaffe41d49d042
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-07-17 17:55:15 +0200

    update

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,26 +8,53 @@ export default {
     components: {
 
     },
-    data(){
+    data() {
         return {
-            store
+            store,
+            apiUrl: "http://localhost:8000/api/",
+            loading: false,
+            loadingError: false,
+            projects: [],
         }
     },
-    methods:{
+    methods: {
+        getProjectsFirstPage() {
 
+            this.loading = true;
+            axios.get(this.apiUrl + "projects").then(response => {
+                console.log(response.data);
+                this.projects = response.data.results;
+                this.projectsCurrentPage = response.data.results.current_page;
+                this.projectsTotalPages = response.data.results.last_page;
+                this.loading = false;
+            }).catch(err => {
+                this.loading = false;
+                this.loadingError = err.message;
+            });
+
+        },
     },
-    mounted(){
+    mounted() {
+        this.getProjectsFirstPage();
     }
 }
 </script>
 
 <template>
-    <div class="container bg-black">
-        <h1 class="text-white">Main</h1>
+    <div class="container d-flex flex-wrap gap-3">
+        <div v-for="project in projects">
+            <div class="card" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.description }}.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../variables.scss' as *;
-
 </style>
